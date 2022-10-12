a75afa55dd323c40e2dde66ac9419449cbfe7bcb
Lexin Gong <gonglexin@gmail.com>
2022-10-12 22:07:18 +0800

    solution for secrets

---
+++ elixir/secrets/lib/secrets.ex
@@ -0,0 +1,32 @@
+defmodule Secrets do
+  def secret_add(secret) do
+    &(&1 + secret)
+  end
+
+  def secret_subtract(secret) do
+    &(&1 - secret)
+  end
+
+  def secret_multiply(secret) do
+    &(&1 * secret)
+  end
+
+  def secret_divide(secret) do
+    &div(&1, secret)
+  end
+
+  import Bitwise
+
+  def secret_and(secret) do
+    &(&1 &&& secret)
+  end
+
+  def secret_xor(secret) do
+    # &(&1 ||| secret)
+    &bxor(&1, secret)
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    &(secret_function2.(secret_function1.(&1)))
+  end
+end
