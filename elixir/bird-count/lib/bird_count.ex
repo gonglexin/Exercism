05979976bbc73d783ab9f13a6f1c6d8aa3eb80c1
Lexin Gong <gonglexin@gmail.com>
2022-10-17 21:55:01 +0800

    Recursion version

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -7,18 +7,14 @@ defmodule BirdCount do
 
   def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  def has_day_without_birds?(list) do
-    list
-    |> Enum.any?(&(&1 == 0))
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _tail]), do: true
+  def has_day_without_birds?([_head | tail]), do: has_day_without_birds?(tail)
 
-  def total(list) do
-    Enum.sum(list)
-  end
+  def total([]), do: 0
+  def total([head | tail]), do: head + total(tail)
 
-  def busy_days(list) do
-    list
-    |> Enum.reject(&(&1 < 5))
-    |> Enum.count()
-  end
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head < 5, do: busy_days(tail)
+  def busy_days([_head | tail]), do: 1 + busy_days(tail)
 end
