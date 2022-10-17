f0bc8cd348b2860a765bd37b2b47c300b9588f18
Lexin Gong <gonglexin@gmail.com>
2022-10-17 20:39:27 +0800

    refact

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -6,36 +6,20 @@ defmodule HighSchoolSweetheart do
   end
 
   def initial(name) do
-    first =
-      name
-      |> first_letter()
-      |> String.upcase()
-
-    "#{first}."
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
   end
 
   def initials(full_name) do
     full_name
     |> String.split()
-    |> Enum.map(&initial(&1))
+    |> Enum.map(&initial/1)
     |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
     pair = "#{initials(full_name1)}  +  #{initials(full_name2)}"
 
     """
