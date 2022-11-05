458859ea77ac14fd0bb81e98cc1440e1445ed08e
Lexin Gong <gonglexin@gmail.com>
2022-11-05 22:01:04 +0800

    code sytle: add a blank line

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -21,6 +21,7 @@ defmodule SpaceAge do
     uranus: 84.016846,
     neptune: 164.79132
   }
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet', or an error if 'planet' is not a planet.
