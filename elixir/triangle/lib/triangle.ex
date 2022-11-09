8c656a2881d99fe550cd8655f649d64ab772e185
Lexin Gong <gonglexin@gmail.com>
2022-11-09 23:04:58 +0800

    refact

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -5,31 +5,17 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
-  def kind(a, a, a) when a > 0 do
-    {:ok, :equilateral}
-  end
-
-  def kind(a, a, c) when a > 0 and c > 0 and a * 2 > c do
-    {:ok, :isosceles}
-  end
-
-  def kind(a, b, a) when a > 0 and b > 0 and a * 2 > b do
-    {:ok, :isosceles}
-  end
-
-  def kind(a, b, b) when a > 0 and b > 0 and b * 2 > a do
-    {:ok, :isosceles}
-  end
-
-  def kind(a, b, c) when a > 0 and b > 0 and c > 0 and a + b > c and a + c > b and b + c > a do
-    {:ok, :scalene}
-  end
-
   def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0 do
     {:error, "all side lengths must be positive"}
   end
 
-  def kind(_, _, _) do
+  def kind(a, b, c) when a + b < c or a + c < b or b + c < a do
     {:error, "side lengths violate triangle inequality"}
   end
+
+  def kind(a, a, a), do: {:ok, :equilateral}
+  def kind(a, a, _), do: {:ok, :isosceles}
+  def kind(a, _, a), do: {:ok, :isosceles}
+  def kind(_, a, a), do: {:ok, :isosceles}
+  def kind(_, _, _), do: {:ok, :scalene}
 end
