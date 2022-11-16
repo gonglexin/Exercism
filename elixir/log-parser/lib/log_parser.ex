90bab460f5af2e1075d78a224bc7f7178c24b3a3
Lexin Gong <gonglexin@gmail.com>
2022-11-16 13:13:44 +0800

    refact

--- elixir/log-parser/lib/log_parser.ex
+++ elixir/log-parser/lib/log_parser.ex
@@ -4,23 +4,17 @@ defmodule LogParser do
   end
 
   def split_line(line) do
-    line
-    |> String.split(~r/<(~|\*|=|-)*>/)
+    String.split(line, ~r/<(~|\*|=|-)*>/)
   end
 
   def remove_artifacts(line) do
-    line
-    |> String.replace(~r/end-of-line\d+/i, "")
+    String.replace(line, ~r/end-of-line\d+/i, "")
   end
 
   def tag_with_user_name(line) do
-    user =
-      (Regex.named_captures(~r/User\s+(?<user>(\S)*)/, line) || %{})
-      |> Map.get("user")
-
-    case user do
+    case Regex.run(~r/User\s+(\S+)/, line) do
       nil -> line
-      user -> "[USER] #{user} #{line}"
+      match -> "[USER] #{Enum.at(match, 1)} #{line}"
     end
   end
 end
