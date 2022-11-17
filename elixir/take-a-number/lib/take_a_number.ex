eeda8a27227ed57ee0c69c45b850cf7a2f0fa5d9
Lexin Gong <gonglexin@gmail.com>
2022-11-17 22:47:41 +0800

    code style

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -8,15 +8,17 @@ defmodule TakeANumber do
       {:report_state, sender_pid} ->
         send(sender_pid, state)
         loop(state)
+
       {:take_a_number, sender_pid} ->
         state = state + 1
         send(sender_pid, state)
         loop(state)
+
       :stop ->
         Process.exit(self(), :kill)
+
       _ ->
         loop(state)
     end
-
   end
 end
