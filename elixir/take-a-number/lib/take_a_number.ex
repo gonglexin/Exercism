ed59b453e7e052628da8b02048cfb4fcf09aa70a
Lexin Gong <gonglexin@gmail.com>
2022-11-17 22:46:32 +0800

    solution for take-a-number

---
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -0,0 +1,22 @@
+defmodule TakeANumber do
+  def start(state \\ 0) do
+    spawn(fn -> loop(state) end)
+  end
+
+  defp loop(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        loop(state)
+      {:take_a_number, sender_pid} ->
+        state = state + 1
+        send(sender_pid, state)
+        loop(state)
+      :stop ->
+        Process.exit(self(), :kill)
+      _ ->
+        loop(state)
+    end
+
+  end
+end
