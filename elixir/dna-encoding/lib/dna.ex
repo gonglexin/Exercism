f0dbcf8be887f6e0d9b6df7c0bb1d059701fc47d
Lexin Gong <gonglexin@gmail.com>
2022-11-21 16:14:53 +0800

    refact & style

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -9,27 +9,12 @@ defmodule DNA do
 
   @code_to_nucleotide @nucleotide_to_code |> Enum.into(%{}, fn {k, v} -> {v, k} end)
 
-  def encode_nucleotide(code_point) do
-    @nucleotide_to_code[code_point]
-  end
+  def encode_nucleotide(code_point), do: @nucleotide_to_code[code_point]
+  def decode_nucleotide(encoded_code), do: @code_to_nucleotide[encoded_code]
 
-  def decode_nucleotide(encoded_code) do
-    @code_to_nucleotide[encoded_code]
-  end
+  def encode([]), do: <<>>
+  def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
 
-  def encode([]) do
-    <<>>
-  end
-
-  def encode([head | tail]) do
-    <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
-  end
-
-  def decode(<<>>) do
-    ''
-  end
-
-  def decode(<<head::4, tail::bitstring>>) do
-    [decode_nucleotide(head)] ++ decode(tail)
-  end
+  def decode(<<>>), do: ''
+  def decode(<<head::4, tail::bitstring>>), do: [decode_nucleotide(head) | decode(tail)]
 end
