b53de4ec686c36bcf900cec82ecd5fc150968323
Lexin Gong <gonglexin@gmail.com>
2023-11-28 10:38:49 +0800

    Update exercise

--- elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -15,14 +15,28 @@ defmodule ResistorColorTrio do
   @doc """
   Calculate the resistance value in ohm or kiloohm from resistor colors
   """
-  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms}
-  def label([c1, c2, c3]) do
-    first = [@colors[c1], @colors[c2]] |> Integer.undigits
+  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
+  def label([c1, c2, c3 | _]) do
+    first = [@colors[c1], @colors[c2]] |> Integer.undigits()
 
     second = @colors[c3]
 
-    ohms = first * :math.pow(10, second) |> round()
+    ohms = (first * :math.pow(10, second)) |> round()
 
-    if ohms < 1000, do: {ohms, :ohms}, else: {div(ohms, 1000), :kiloohms}
+    s = ohms |> to_string()
+
+    cond do
+      s |> String.ends_with?("000000000") ->
+        {div(ohms, 1_000_000_000), :gigaohms}
+
+      s |> String.ends_with?("000000") ->
+        {div(ohms, 1_000_000), :megaohms}
+
+      s |> String.ends_with?("000") ->
+        {div(ohms, 1000), :kiloohms}
+
+      true ->
+        {ohms, :ohms}
+    end
   end
 end
